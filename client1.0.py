d64471f77ddd1a1dd44293ec1f56cdb99156acab
galliacode <hperfomancedev@gmail.com>
2024-04-23 21:18:54 -0400

    v2.0

---
+++ client1.0.py
@@ -0,0 +1,186 @@
+import threading
+import time
+from selenium import webdriver
+import json
+from selenium.webdriver.common.by import By
+from datetime import datetime
+import requests
+import tkinter as tk
+from selenium.webdriver.common.action_chains import ActionChains
+
+driver = webdriver.Chrome()
+driver.get("https://tradingview.com/")
+
+
+def load_session_id():
+    try:
+        with open("session_id.json", "r") as file:
+            data = json.load(file)
+            return data.get("session_id", "")
+    except FileNotFoundError:
+        return ""
+
+
+def save_session_id():
+    session_id.set(entry.get())
+    with open("session_id.json", "w") as file:
+        json.dump({"session_id": session_id}, file)
+    print("Session ID saved successfully.")
+
+
+# Define your cookie
+cookie = {
+    'name': 'sessionid',
+    'value': load_session_id(),
+    'path': '/',
+    'domain': '.tradingview.com',  # Change to the domain of the website
+    'secure': True,
+    'httpOnly': True
+}
+
+driver.add_cookie(cookie)
+
+driver.get("https://tradingview.com/markets/currencies/rates-all/")
+alert_list = []
+
+# element_list = []
+alive = True
+
+
+def load_session_id():
+    try:
+        with open("session_id.json", "r") as file:
+            data = json.load(file)
+            return data.get("session_id", "")
+    except FileNotFoundError:
+        return ""
+
+
+def save_session_id():
+    session_id.set(entry.get())
+    with open("session_id.json", "w") as file:
+        json.dump({"session_id": session_id}, file)
+    print("Session ID saved successfully.")
+
+
+def process_browser_log_entry(entry):
+    response = json.loads(entry['message'])['message']
+    return response
+
+
+# Define the server URL where you want to send the data
+server_url = 'http://localhost:5000/receive_data'
+
+
+def monitor_requests():
+    global alert_list
+    # global element_list
+    try:
+        while True and alive:
+            # Process requests that have been captured
+            alert = None
+            try:
+                alert = driver.find_element(By.CLASS_NAME, 'message-PQUvhamm')
+            except:
+                pass
+            if alert:
+                try:
+                    message = alert.text
+                    elements = message.split("\n")
+
+                    parent = alert.parent
+                    for entry in entry_list:
+                        print(f"Trying ELEMENT -> {elements} for PORT : {entry}")
+                        response = requests.post(f"http://127.0.0.1:{entry}/receive_data", data={'data': elements})
+                        # Check the response from the server
+                        if response.status_code == 200:
+                            print("Data sent to server successfully.")
+                            while True and alive:
+                                try:
+                                    close_button = parent.find_element(By.XPATH,
+                                                                       "/html/body/div[2]/div/div[2]/div[1]/div[2]/div[2]/div[2]/div/div[2]/div[2]/div[4]/div")
+                                    hover = ActionChains(driver).move_to_element(close_button)
+                                    hover.perform()
+                                    close_button.click()
+                                except:
+                                    print("No more alerts left")
+                                else:
+                                    break
+                        else:
+                            print("Error sending data to server:", response.status_code)
+                except:
+                    pass
+            else:
+                pass
+            try:
+                button = driver.find_element(By.XPATH,
+                                             "/html/body/div[6]/div[2]/div/div[2]/div/div/div[1]/div/div/div/div[3]/div/button[1]")
+                button.click()
+            except Exception as e:
+                pass
+
+    except KeyboardInterrupt:
+        print("Stopping monitoring...")
+
+
+# Run the monitoring in a background thread
+thread = threading.Thread(target=monitor_requests)
+thread.start()
+
+# Create main window
+root = tk.Tk()
+root.title("Client")
+root.geometry("300x350")
+
+session_id = tk.StringVar()
+
+# Entry field for session ID
+entry = tk.Entry(root)
+entry.insert(0, session_id.get())
+entry.pack(pady=10)
+
+# Button to save session ID
+button = tk.Button(root, text="Save Session ID", command=save_session_id)
+button.pack()
+
+default_value = 5001
+entry_list = [str(default_value)]
+
+
+def add_entry():
+    global default_value
+    if default_value == 5004:
+        return
+    default_value += 1
+    entry_list.append(str(default_value))
+    update_listbox()
+
+
+def update_listbox():
+    listbox.delete(0, tk.END)
+    for entry in entry_list:
+        listbox.insert(tk.END, entry)
+
+
+tk.Label(text="Ports").pack()
+# Create listbox to display entries
+listbox = tk.Listbox(root)
+listbox.pack(pady=10)
+
+add_button = tk.Button(root, text="Add Entry", command=add_entry)
+add_button.pack()
+
+# Add initial entry
+update_listbox()
+
+print("Monitoring started. Press Ctrl+C to stop.")
+try:
+    root.mainloop()
+except KeyboardInterrupt:
+    print("Stopping script...")
+    alive = False
+
+alive = False
+
+# Cleanup
+driver.quit()
---
+++ server - t2.py
@@ -0,0 +1,284 @@
+import threading
+import flask
+from flask import Flask, request, jsonify
+import MetaTrader5 as mt5
+import re
+import tkinter as tk
+from tkinter import filedialog
+from datetime import datetime, timedelta
+import numpy
+import pandas
+
+name = 'main'
+app = Flask(name)
+
+comment_list = []
+
+# Initialize MetaTrader 5 connection
+if not mt5.initialize(path='t2/terminal64.exe'):
+    print("Initialize() failed, error code =", mt5.last_error())
+    quit()
+
+
+@app.route('/receive_data', methods=['POST'])
+def receive_data():
+    data_str = request.form['data']
+    data_arr = data_str.split(',')
+    print(f"Received data: {data_arr}")
+    print("Data Type", type(data_arr))
+
+    # Here you can process the data and make orders with MT5 based on your strategy
+    # For demonstration, we'll just log the data received
+    process_data_and_place_order(data_arr)
+
+    return jsonify({'status': 'success', 'message': 'Data received'}), 200
+
+
+def process_data_and_place_order(data):
+    global reverse, extracted_text, use_file, lot_dict
+    # Parse the data
+    # Example: parsed_data = parse_data(data)
+
+    # Check if MT5 terminal is connected
+    if not mt5.terminal_info():
+        print("MT5 terminal is not connected")
+        return
+
+    symbol = data[0].strip()
+    print(symbol)
+    if symbol in sym_dict and use_conv.get():
+        # Replace the variable with its corresponding value
+        symbol = result_dict[symbol]
+
+    selected = mt5.symbol_select(symbol, True)
+    if not selected:
+        print("Failed to select " + symbol)
+        return
+
+    symbol_info = mt5.symbol_info(symbol)
+    if symbol_info is None:
+        print(symbol, "not found, can not call order_check()")
+        return
+
+    # if the symbol is unavailable in MarketWatch, add it
+    if not symbol_info.visible:
+        print(symbol, "is not visible, trying to switch on")
+        if not mt5.symbol_select(symbol, True):
+            print("symbol_select({}}) failed, exit", symbol)
+            return
+
+    lot = 0.1
+    if symbol in lot_dict and use_lot.get():
+        lot = float(lot_dict[symbol])
+    tick = mt5.symbol_info_tick(symbol)
+    print(tick)
+    point = tick.bid
+    price = tick.ask
+    print(price)
+    order_type = 0 if data[1].strip() == "buy" else 1
+
+    # Reverse the order type based on the option
+    if reverse.get():
+        order_type = 1 if order_type == 0 else 0
+
+    sl = round(price - (0.01 * price), 5) if order_type == 0 else round(point + (0.01 * price), 5)
+    tp = round(price + (0.01 * price), 5) if order_type == 0 else round(point - (0.01 * price), 5)
+    print(f"Lot size = {lot}")
+    print("Stop Loss = " + str(sl))
+    print("Take Profit = " + str(tp))
+    print("Order type : " + str(order_type))
+
+    comment = re.split('=', data[5].strip(), maxsplit=1)[1]
+    start = 0
+    end = 0
+    if len(data) == 7:
+        timeframe = re.split('=', data[6].strip(), maxsplit=1)[1]
+        start = re.split('-', timeframe, maxsplit=1)[0]
+        end = re.split('-', timeframe, maxsplit=1)[1]
+
+    ret_logic = True
+    comment_list = []
+
+    # Now we get all positions from the Terminal
+    positions = mt5.positions_get(symbol=symbol)
+
+    # Now extract the list of comments
+    if positions:
+        comment_list = [pos.comment for pos in positions]
+    print(comment_list)
+    if comment in comment_list:
+        if positions:
+            print(positions)
+            for pos in positions:
+                if comment == pos.comment:
+                    ticket = pos.ticket
+                    if pos.type == order_type:
+                        print("---------\nOrder with same comment AND order type is present\n---------")
+                        return
+                    print("Going to close this existing order with ticket " + str(ticket) + " and symbol: " + symbol)
+                    mt5.Close(symbol=symbol, ticket=ticket)
+                    print("Removed the order")
+                    if len(data) == 7:
+                        print("Proceeding to check for ET conditions")
+                        order_time = datetime.fromtimestamp(pos.time)
+                        # time = pandas.DataFrame({'time':[order_time]})
+                        # time['time'] = pandas.to_datetime(time['time'], unit='s')
+
+                        current_time = datetime.now()
+                        print(f"Order Time = {order_time}")
+                        print(f"Current Time = {current_time}")
+                        delta = current_time - order_time + timedelta(hours=3)
+                        print(f"Delta time = {delta}")
+                        start = timedelta(hours=int(start))
+                        end = timedelta(hours=int(end))
+                        print(f"Start Delta = {start}")
+                        print(f"End Delta = {end}")
+                        if start < delta < end or start == end:
+                            ret_logic = False
+                            print("Order to be reversed")
+                    if ret_logic:
+                        comment_list.remove(comment)
+                        return
+        if ret_logic:
+            print("Order with same comment is present")
+            return
+
+    if extracted_text and use_file.get():
+        if comment in extracted_text:
+            pass
+        else:
+            print("Not found in provided list")
+            return
+
+    risk = int(re.split('=', data[3].strip(), maxsplit=1)[1])
+    magic = int(re.split('=', data[4].strip(), maxsplit=1)[1])
+    print("Comment = " + comment)
+    request = {
+        "action": mt5.TRADE_ACTION_DEAL,
+        "symbol": symbol,
+        "volume": lot,
+        "type": order_type,
+        "price": price if order_type == 0 else point,
+        "sl": sl,
+        "tp": tp,
+        "magic": magic,
+        "risk": risk,
+        "comment": comment,
+        "type_time": mt5.ORDER_TIME_GTC,
+        "type_filling": mt5.ORDER_FILLING_IOC,
+    }
+    result = mt5.order_send(request)  # Error here
+    print(result)
+    if result.retcode != mt5.TRADE_RETCODE_DONE:
+        print("Failed to send order :(", result.retcode)
+    else:
+        print("Order placed successfully!")
+
+    # Implement your own logic here based on the data
+    pass
+
+
+def start_server():
+    app.run(debug=False, port=5001, threaded=True)
+
+
+extracted_text = []
+sym_dict = {}
+lot_dict = {}
+server_thread = threading.Thread(target=start_server)
+server_thread.start()
+
+# Setting up the GUI using Tkinter
+
+# Create main window
+root = tk.Tk()
+title = tk.StringVar()
+account = mt5.account_info()
+title.set(f"Alert Bridge : {account.login}|{account.name}|{account.server}")
+root.title(title.get())
+root.geometry("350x400")
+root.grid()
+
+# Create a Checkbutton widget
+reverse = tk.BooleanVar()
+reverse.set(False)
+check_button = tk.Checkbutton(root, text="Reverse", variable=reverse)
+check_button.pack()
+
+
+def browse_file():
+    global extracted_text
+    filename = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
+    if filename:
+        with open(filename, 'r') as file:
+            text = file.read()
+            extracted_text = [s.strip() for s in text.split(',')]
+            display_text.config(text=f"Comments : {extracted_text}")
+
+
+def browse_file_sym():
+    global sym_dict
+    filename = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
+    if filename:
+        with open(filename, 'r') as file:
+            text = file.read()
+            extracted_text_sym = [s.strip() for s in text.split(',')]
+            sym_dict = dict(item.split('=') for item in extracted_text_sym)
+            display_text_sym.config(text=f"{sym_dict}")
+
+
+def browse_file_lot():
+    global lot_dict
+    filename = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
+    if filename:
+        with open(filename, 'r') as file:
+            text = file.read()
+            extracted_text_sym = [s.strip() for s in text.split(',')]
+            lot_dict = dict(item.split('=') for item in extracted_text_sym)
+            display_text_lot.config(text=f"{lot_dict}")
+
+
+# Creating a button to browse for a file
+browse_button = tk.Button(root, text="Browse File", command=browse_file)
+browse_button.pack(pady=10)
+
+# Create a Checkbutton widget for comments file
+use_file = tk.BooleanVar()
+use_file.set(False)
+check_button_comment = tk.Checkbutton(root, text="Use Comments File", variable=use_file)
+check_button_comment.pack()
+
+# Label to display the extracted text
+display_text = tk.Label(root, text="")
+display_text.pack(pady=10)
+
+# Creating a button to browse for a file
+browse_button_sym = tk.Button(root, text="Select Symbol File", command=browse_file_sym)
+browse_button_sym.pack(pady=10)
+
+# Create a Checkbutton widget for Symbols Conversion file
+use_conv = tk.BooleanVar()
+use_conv.set(False)
+check_button_conv = tk.Checkbutton(root, text="Use Conversion File", variable=use_conv)
+check_button_conv.pack()
+
+# Label to display the extracted text
+display_text_sym = tk.Label(root, text="")
+display_text_sym.pack(pady=10)
+
+# Creating a button to browse for a file
+browse_button_lot = tk.Button(root, text="Select Lot File", command=browse_file_lot)
+browse_button_lot.pack(pady=10)
+
+# Create a Checkbutton widget for Lot setting file
+use_lot = tk.BooleanVar()
+use_lot.set(True)
+check_button_lot = tk.Checkbutton(root, text="Use Lot File", variable=use_lot)
+check_button_lot.pack()
+
+# Label to display the extracted text
+display_text_lot = tk.Label(root, text="")
+display_text_lot.pack(pady=10)
+
+# Run the Tkinter event loop
+root.mainloop()
